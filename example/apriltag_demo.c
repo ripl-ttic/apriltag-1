6e02c42bba5d6a3be70a0f459dec3fa9230d5ba0
alexushatten <alexander.hatteland@gmail.com>
2020-06-26 16:52:05 +0200

    center of tag print

--- example/apriltag_demo.c
+++ example/apriltag_demo.c
@@ -278,6 +278,7 @@ int main(int argc, char *argv[])
 
                 hamm_hist[det->hamming]++;
                 total_hamm_hist[det->hamming]++;
+                printf("centerpose: %f,%f \n",det->c[0],det->c[1]);
             }
 
             apriltag_detections_destroy(detections);
